b83313307c0b2923fbe8edb93456ad4cdcc2b617
Dmitry <d@kozlov.ru>
2025-06-23 21:13:49 +0300

    llm/local: Added stream

--- index.ts
+++ index.ts
@@ -1,4 +1,15 @@
 import ollama from 'ollama'
+import type { ChatRequest } from 'ollama'
 import { Service } from 'hub-service'
 
-new Service().post('llm/local', ({ body }) => ollama.chat(body)).start()
+type StreamRequest = ChatRequest & { stream: true }
+
+new Service()
+  .post('llm/local', body => ollama.chat(body))
+  .stream('llm/local', async function* (body) {
+    const response = await ollama.chat(body as StreamRequest)
+    for await (const chunk of response) {
+      yield chunk
+    }
+  })
+  .start()
