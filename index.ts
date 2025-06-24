af78a853b6a0ecf6e405d97a26601640c6afe2df
Dmitry <d@kozlov.ru>
2025-06-24 12:35:46 +0300

    llm/local: Forced stream boolean to true/false

--- index.ts
+++ index.ts
@@ -8,10 +8,12 @@ type StreamRequest = ChatRequest & { stream: true }
 new Service()
   .post('llm/local', async (body: PostRequest) => {
     await ollama.pull({ model: body.model })
+    body.stream = false
     return await ollama.chat(body)
   })
   .stream('llm/local', async function* (body: StreamRequest) {
     await ollama.pull({ model: body.model })
+    body.stream = true
     const response = await ollama.chat(body)
     for await (const chunk of response) {
       yield chunk
